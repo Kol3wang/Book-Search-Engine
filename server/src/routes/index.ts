b091dbf26ae31573470b8a8be5b57c96ab584c17
kol <kol3wang@gmail.com>
2024-10-30 20:13:00 -0400

    Fix CommonJS compatibility for deployment on Render

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,16 +1,17 @@
 import type { Request, Response } from 'express';
 import express from 'express';
+import path from 'path';
+import apiRoutes from './api/index.js';
+
 const router = express.Router();
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
+// Define __dirname and __filename for CommonJS compatibility
+const __filename = path.resolve();
 const __dirname = path.dirname(__filename);
-import apiRoutes from './api/index.js';
 
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
+// Serve up React front-end in production
 router.use((_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
